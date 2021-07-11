e7f4d2ccd35c2ad5c6446e48190a47c262556666
breakingbar <juliusz@muskala.eu>
2021-07-11 19:20:41 +0200

    initial commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'modern-normalize/modern-normalize.css'
-import './assets/index.css'
-import Ideas from './Ideas'
-
-ReactDOM.render(<Ideas />,document.getElementById('root'))
